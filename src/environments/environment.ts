116d8198edcba1f5ceaa0e7775871ca9282dd103
melissapanduroalbino <melissa.panduroa@gmail.com>
2024-11-28 08:41:51 +0000

    On branch develop Your branch is up to date with 'origin/develop'.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,6 +8,7 @@ export const environment = {
     chatbot_be: '${API_BACKEND}'
   };
   
+
   /*
    * For easier debugging in development mode, you can import the following file
    * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
