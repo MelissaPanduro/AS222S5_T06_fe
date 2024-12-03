26ce127b308481e3f2b645c721904c54d98a5104
Melissa Panduro <127220575+MelissaPanduro@users.noreply.github.com>
2024-12-03 21:46:05 +0000

    actualizacion3

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,4 @@
-entrypoint.sh
-#!/bin/sh
-
+#!/bin/s
 # Reemplazar el valor de API_BACKEND en environment.ts
 sed -i "s|http://localhost:8080|${API_BACKEND}|g" /app/src/environments/environment.ts
 
